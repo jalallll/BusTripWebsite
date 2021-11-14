30dc6c265dfa4184637ebf71b9357c39c9a8436a
Jalal <jalalq11@gmail.com>
2021-11-14 06:02:00 -0500

    test form

--- editdata.php
+++ editdata.php
@@ -24,18 +24,19 @@ $input_end_date = $_POST["input_end_date"];
 
 if($input_trip_id != ""){
     $query = "UPDATE bustrip set tripname='$input_trip_name', startdate='$input_start_date', enddate='$input_end_date' WHERE tripid='$input_trip_id'";
-}
-$result = mysqli_query($connection,$query);
-if (!$result) {
-    die("databases query failed.");
-}
-if ($result ){
-    echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
+    $result = mysqli_query($connection,$query);
+    if (!$result) {
+        die("databases query failed.");
     }
-    mysqli_free_result($result);
-    echo "</table>";
+    if ($result ){
+        echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
+        }
+        mysqli_free_result($result);
+        echo "</table>";
+    }
 }
+
 ?>
