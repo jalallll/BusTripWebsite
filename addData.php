b0e5b2db997b096e42177b38783c509992a8a06b
Jalal <jalalq11@gmail.com>
2021-11-14 08:36:48 -0500

    test add

--- addData.php
+++ addData.php
@@ -10,7 +10,7 @@
     $license_plate = $_POST['input_license_plate'];
     $largest_id_query = "SELECT MAX(tripid) FROM bustrip";
     $largest_id = mysqli_query($connection, $largest_id_query);
-    $new_id = $largest_id + 1;
+    $new_id = 34;
     $add_data_query = "INSERT INTO bustrip (tripid, tripname, startdate, enddaste, country, licenseplatenumber) VALUES ('$new_id','$name', '$start', '$end', '$country', '$license_plate')";
     $add_data_res = mysqli_query($connection, $add_data_query);
 
