778b6eb182c4668ff521afa579d6a1d7cc859c49
Jalal <jalalq11@gmail.com>
2021-11-14 05:22:26 -0500

    test

--- order.php
+++ order.php
@@ -1,5 +1,22 @@
 <?php
 
+ $dbhost = "localhost";
+ $dbuser= "root";
+ $dbpass = "cs3319";
+ $dbname = "04_assign2db";
+ $connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
+if (mysqli_connect_errno()) {
+     die("database connection failed :" .
+     mysqli_connect_error() .
+     "(" . mysqli_connect_errno() . ")"
+         );
+    }
+
+
+
+
+
+
 $radio_btn_val = $_POST["radio_btn"];
 
 
