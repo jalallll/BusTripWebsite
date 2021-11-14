0d84b177959b8f0dd95071b8169b4cae0d08f084
Jalal <jalalq11@gmail.com>
2021-11-14 06:06:36 -0500

    cleaned editdate.php file

--- editdata.php
+++ editdata.php
@@ -11,11 +11,6 @@ if (mysqli_connect_errno()) {
          );
     }
 
-
-
-
-
-
 $input_trip_id = $_POST["input_trip_id"];
 $input_trip_name = $_POST["input_trip_name"];
 $input_start_date = $_POST["input_start_date"];
@@ -32,18 +27,5 @@ if($input_trip_id != ""){
         echo "Successful update";
     }
 }
-$query = "SELECT * FROM bustrip";
-    $result = mysqli_query($connection,$query);
-    if (!$result) {
-        die("databases query failed.");
-    }
-    if ($result ){
-        echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
 
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
-        }
-        mysqli_free_result($result);
-        echo "</table>";
-    }
 ?>
