cc7001ac47ec0f0e24ca6cde83db78d84f384caf
Jalal <jalalq11@gmail.com>
2021-11-14 10:08:40 -0500

    delete record test

---
+++ manage.php
@@ -0,0 +1,23 @@
+<?php
+ $dbhost = "localhost";
+ $dbuser= "root";
+ $dbpass = "cs3319";
+ $dbname = "04_assign2db";
+ $connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
+if (mysqli_connect_errno()) {
+     die("database connection failed :" .
+     mysqli_connect_error() .
+     "(" . mysqli_connect_errno() . ")"
+         );
+    }
+
+
+
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $connection->query("DELETE FROM bustrip WHERE id='$id'");
+
+}
+
+
+?>
