5b34e229514877f6178127baae3353f33f6949bc
Jalal <jalalq11@gmail.com>
2021-11-14 04:36:52 -0500

    fixed html table

--- getdata.php
+++ getdata.php
@@ -5,17 +5,10 @@ $result = mysqli_query($connection,$query);
 if (!$result) {
     die("databases query failed.");
 }
-echo "<table>";
-echo "<th>Tripid</th>";
-echo "<th>Trip Name</th>";
-echo "<th>Start Date</th>";
-echo "<th>End Date</th>";
-echo "<th>Country</th>";
-echo "<th>License Plate</th>";
-
+echo "<table> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th>";
 
 while ($row = mysqli_fetch_assoc($result)) {
-     echo "<tr>" . "<td>" . $row['tripid'] . "</td>" . "<td>" . $row['tripname'] . "</td>" .  "<td>" . $row['startdate'] . "/<td>" . "<td>" . $row['enddate'] . "</td>" . "<td>" . $row['country'] . "</td>" . "<td>" . $row['licenseplatenumber'] . "</td>" . "</tr>" ;
+     echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
 }
 mysqli_free_result($result);
 echo "</table>";
