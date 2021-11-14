d7541ff141b00e93de269cfbfd7118b52d4fe05c
Jalal <jalalq11@gmail.com>
2021-11-14 04:32:07 -0500

    html table test

--- getdata.php
+++ getdata.php
@@ -5,9 +5,18 @@ $result = mysqli_query($connection,$query);
 if (!$result) {
     die("databases query failed.");
 }
+echo "<table>";
+echo "<th>Tripid</th>";
+echo "<th>Trip Name</th>";
+echo "<th>Start Date</th>";
+echo "<th>End Date</th>";
+echo "<th>Country</th>";
+echo "<th>License Plate</th>";
+
+
 while ($row = mysqli_fetch_assoc($result)) {
-     var_dump($row);
-     echo $row;
+     echo "<tr>" . "<td>" . $row['tripid'] . "</td>" . "<td>" . $row['tripname'] . "</td>" .  "<td>" . $row['startdate'] . "/<td>" . "<td>" . $row['enddate'] . "</td>" . "<td>" . $row['country'] . "</td>" . "<td>" . $row['licenseplatenumber'] . "</td>" . "</tr>" ;
 }
 mysqli_free_result($result);
+echo "</table>";
 ?>
