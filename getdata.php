f68106fa2e4c46f7f6203d0226a522c17cf9e5e6
Jalal <jalalq11@gmail.com>
2021-11-14 04:38:21 -0500

    fixed html table

--- getdata.php
+++ getdata.php
@@ -5,7 +5,7 @@ $result = mysqli_query($connection,$query);
 if (!$result) {
     die("databases query failed.");
 }
-echo "<table> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th>";
+echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
 
 while ($row = mysqli_fetch_assoc($result)) {
      echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
