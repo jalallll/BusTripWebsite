52af5dbb26d7ce1b23e10cd7ab6a462dcb4223bc
Jalal <jalalq11@gmail.com>
2021-11-14 09:56:50 -0500

    changed table layout

--- getdata.php
+++ getdata.php
@@ -7,9 +7,40 @@ if (!$result) {
 }
 echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
 
-while ($row = mysqli_fetch_assoc($result)) {
-     echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
-}
+while ($row = mysqli_fetch_assoc($result)): ?>
+    <tr>
+        <td>
+            <?php echo $row['tripid']; ?>
+        </td>
+        <td>
+            <?php echo $row['tripname']; ?>
+        </td>
+        <td>
+            <?php echo $row['startdate']; ?>
+        </td>
+        <td>
+            <?php echo $row['enddate']; ?>
+        </td>
+        <td>
+            <?php echo $row['country']; ?>
+        </td>
+        <td>
+            <?php echo $row['licenseplatenumber']; ?>
+        </td>
+        <td>
+            <a href="manage.php?delete=<?php echo $row['tripid']; ?>">Delete</a>
+        </td>
+        <td>
+            <a href="manage.php?update=<?php echo $row['tripid']; ?>">Update</a>
+        </td>
+
+        </td>
+    </tr>
+
+
+<?php endwhile; ?>
+
+<?php
 mysqli_free_result($result);
 echo "</table>";
 ?>
