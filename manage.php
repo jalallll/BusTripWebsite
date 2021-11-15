bdc172bb8ae13c9eff9340cfb9a3bfcf37420522
Jalal <jalalq11@gmail.com>
2021-11-15 15:33:25 -0500

    fetch trips by country

--- manage.php
+++ manage.php
@@ -7,8 +7,6 @@ require "Database.php";
 $DB = Database::connect();
 
 
-
-
 if(isset($_GET['delete'])){
     $id = $_GET['delete'];
     $response = $DB->query("DELETE FROM bustrip WHERE tripid='$id'");
@@ -40,41 +38,53 @@ if(isset($_GET['delete'])){
 }
 if(isset($_POST['order-by'])){
     $country_btn_val = $_POST["country_btn"];
+    $trip_name_btn_val = $_POST["trip_name_btn"];
 
-$trip_name_btn_val = $_POST["trip_name_btn"];
-
-
-
-
-if($country_btn_val == "Ascending" && $trip_name_btn_val == "Ascending"){
-    echo "Order By: Country (ASC), Trip Name (ASC) <br>";
-    $query = "SELECT * FROM bustrip ORDER BY country ASC, tripname ASC";
-}
-else if($country_btn_val == "Descending" && $trip_name_btn_val == "Ascending"){
-    echo "Order By: Country (DSC), Trip Name (ASC) <br>";
-    $query = "SELECT * FROM bustrip ORDER BY country DESC , tripname ASC";
-}
-else if($country_btn_val == "Descending" && $trip_name_btn_val == "Descending"){
-    echo "Order By: Country (DSC), Trip Name (DSC) <br>";
-    $query = "SELECT * FROM bustrip ORDER BY country DESC , tripname DESC";
-}
-else if($country_btn_val == "Ascending" && $trip_name_btn_val == "Descending"){
-    echo "Order By: Country (ASC), Trip Name (DSC) <br>";
-    $query = "SELECT * FROM bustrip ORDER BY country ASC , tripname DESC";
-}
-$result = $DB->query($query);
-if (!$result) {
-    die("databases query failed.");
-}
-if ($result ){
-    echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
+    if($country_btn_val == "Ascending" && $trip_name_btn_val == "Ascending"){
+        echo "Order By: Country (ASC), Trip Name (ASC) <br>";
+        $query = "SELECT * FROM bustrip ORDER BY country ASC, tripname ASC";
     }
-    mysqli_free_result($result);
-    echo "</table>";
+    else if($country_btn_val == "Descending" && $trip_name_btn_val == "Ascending"){
+        echo "Order By: Country (DSC), Trip Name (ASC) <br>";
+        $query = "SELECT * FROM bustrip ORDER BY country DESC , tripname ASC";
+    }
+    else if($country_btn_val == "Descending" && $trip_name_btn_val == "Descending"){
+        echo "Order By: Country (DSC), Trip Name (DSC) <br>";
+        $query = "SELECT * FROM bustrip ORDER BY country DESC , tripname DESC";
+    }
+    else if($country_btn_val == "Ascending" && $trip_name_btn_val == "Descending"){
+        echo "Order By: Country (ASC), Trip Name (DSC) <br>";
+        $query = "SELECT * FROM bustrip ORDER BY country ASC , tripname DESC";
+    }
+    $result = $DB->query($query);
+    if (!$result) {
+        die("databases query failed.");
+    }
+    if ($result ){
+        echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
+        }
+        mysqli_free_result($result);
+        echo "</table>";
+    }
 }
+if(isset($_POST['fetch_trips_by_country'])){
+    $country = $_POST['select_country'];
+    $result = $DB->query("SELECT * FROM bustrip WHERE country='$country'");
+    if (!$result) {
+        die("databases query failed.");
+    }
+    if ($result ){
+        echo "<table> <tr> <th>Tripid</th> <th>Trip Name</th> <th>Start Date</th> <th>End Date</th> <th>Country</th> <th>License Plate</th> </tr>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr> <td>" . $row['tripid'] . "</td> <td>" . $row['tripname'] . "</td> <td>" . $row['startdate'] . "/<td> <td>" . $row['enddate'] . "</td> <td>" . $row['country'] . "</td> <td>" . $row['licenseplatenumber'] . "</td> </tr>" ;
+        }
+        mysqli_free_result($result);
+        echo "</table>";
+    }
 }
 
 ?>
