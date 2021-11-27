a7c06d21dd09abced5fd5d1ec99ba676836c92e7
Jalal <jalalq11@gmail.com>
2021-11-26 22:27:42 -0500

    layout change

--- main.php
+++ main.php
@@ -15,16 +15,9 @@ session_start();
 	</head>
 	<body>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+		<?php require_once 'manage.php';?>
 		<h1>Assignment 3</h1>
 		<br>
-		<h1>Trips Table</h1>
-		<?php
-		
-		include_once 'getdata.php';
-		require_once 'manage.php';
-
-		?>
 		<form method="POST" action="manage.php">
 			<h2>Order By Country:</h2>
 			<input type="radio" name="country_btn" value="Ascending" checked>Ascending
@@ -36,6 +29,9 @@ session_start();
 			<input type="submit" value="Result" name="order-by"> 
 		</form>
 		<br> 
+		<h1>Trips Table</h1>
+		<?php include_once 'getdata.php';?>
+		<br>
 		<br>
 		<h2>Add New Bus Trip</h2> 
 		<form method="POST" action="manage.php">
