345241da8b89429f130478cba046d09abf8fc0e2
Jalal <jalalq11@gmail.com>
2021-11-26 22:17:58 -0500

    change img h and w

--- getdata.php
+++ getdata.php
@@ -45,7 +45,7 @@ while ($row = mysqli_fetch_assoc($result)): ?>
             <?php echo $row['licenseplatenumber']; ?>
         </td>
         <td>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 12rem; height: 12rem;">
                 <img src="<?php echo $row['urlimage']; ?>" alt="...">
             </div>
         </td>
